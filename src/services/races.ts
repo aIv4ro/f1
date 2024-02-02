2d9311dd3062c30e4dd4b1d4aeef17514ff9ca89
aIv4ro <82459825+aIv4ro@users.noreply.github.com>
2024-02-02 11:32:40 +0100

    use f1-2024 json

--- src/services/races.ts
+++ src/services/races.ts
@@ -1,9 +1,9 @@
-import data from '../../data/f1-2023.json'
+import f1Data from '../../data/f1-2024.json'
 import f2Data from '../../data/f2-2023.json'
 import f3Data from '../../data/f3-2023.json'
 import { Race } from '../types/f1/race.model'
 import { F2Race } from '../types/f2/f2-race.model'
 
-export const races = data.races.map(race => new Race(race))
+export const races = f1Data.races.map(race => new Race(race))
 export const f2Races = f2Data.races.map(race => new F2Race(race))
 export const f3Races = f3Data.races.map(race => new F2Race(race))
